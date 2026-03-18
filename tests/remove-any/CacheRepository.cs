54e04abf89e60e03caf3b1771eee8f54b2a47e82
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2026-03-18 16:31:30 +0100

    explicit

--- tests/remove-any/CacheRepository.cs
+++ tests/remove-any/CacheRepository.cs
@@ -11,7 +11,7 @@ public class CacheRepository
     public void RemoveExpired(TimeSpan timespan)
     {
         var expiredDate = DateTime.UtcNow.Add(-timespan);
-        var expiredEntries = DbSet.Where(x => x.Created < expiredDate);
+        IQueryable<CacheEntry> expiredEntries = DbSet.Where(x => x.Created < expiredDate);
         if (!expiredEntries.Any())
         {
             return;
